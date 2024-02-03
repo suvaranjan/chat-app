7299bb09545ff0309fb593c0b2f2296ac6c0e611
suvaranjan <suvaranjan01@gmail.com>
2024-02-03 13:05:00 +0530

    updated new render setting

--- client/src/components/socket.js
+++ client/src/components/socket.js
@@ -1,6 +1,7 @@
 // socket.js
 import { io } from "socket.io-client";
 
-const socket = io("https://chat-app-busd.onrender.com/");
+const socket = io("http://localhost:10000");
+// const socket = io("https://chat-app-busd.onrender.com/");
 
 export default socket;
